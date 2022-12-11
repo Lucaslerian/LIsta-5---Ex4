3e6a8dd443060ae6e5ab779c02b52d3e7fa418bd
Lucaslerian <lucaslerian@hotmail.com>
2022-12-11 13:57:28 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class App {
+    /*
+     * Faça um programa, com uma função que necessite de um argumento.
+     * A função retorna o valor de caractere ‘P’, se seu argumento for positivo, e
+     * ‘N’, se seu argumento
+     * for zero ou negativo.
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Por favor, entre com um argumento: ");
+        double a = teclado.nextDouble();
+        System.out.println(P_ou_N(a));
+        teclado.close();
+
+    }
+
+    public static String P_ou_N(double a) {
+        String b;
+        if (a <= 0) {
+            b = "N";
+        } else {
+            b = "P";
+        }
+        return b;
+
+    }
+
+}
